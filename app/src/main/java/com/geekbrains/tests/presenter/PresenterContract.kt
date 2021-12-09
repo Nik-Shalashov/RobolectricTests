d5e0fd52edbc8c6410272d6c5deb79f018171f47
Nik-Shalashov <monitor1@inbox.ru>
2021-12-09 20:32:00 +0700

    Добавил методы onAttach, onDetach

--- app/src/main/java/com/geekbrains/tests/presenter/PresenterContract.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/PresenterContract.kt
@@ -3,4 +3,8 @@ package com.geekbrains.tests.presenter
 import com.geekbrains.tests.view.ViewContract
 
 internal interface PresenterContract {
+
+    fun onAttach(viewContract: ViewContract)
+
+    fun onDetach(viewContract: ViewContract)
 }
